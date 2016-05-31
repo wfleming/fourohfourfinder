aeb0b7867a9e14061063e3cdc53ae45273485826
Will Fleming <will@flemi.ng>
2016-05-31 00:18:34 -0400

    Remove action plug from API Controller
    This was cargo coding apparently only applicable to older versions of
    Phoenix: newer code does this automatically, so having this in was
    causing "duplicate render"-type errors.

--- web/controllers/api/page_controller.ex
+++ web/controllers/api/page_controller.ex
@@ -1,8 +1,6 @@
 defmodule FourOhFourFinderApp.Api.PageController do
   use FourOhFourFinderApp.Web, :controller
 
-  plug :action
-
   def fetch(conn, params) do
     page_info = FourOhFourFinderApp.PageAnalyzer.analyze(params["url"])
     render conn, page: page_info
